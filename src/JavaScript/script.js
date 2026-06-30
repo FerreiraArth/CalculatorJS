56fcbcda51b90c3f77395561b38eb9979aac2bfa
FerreiraArth <arthurferreir4a4@gmail.com>
2026-06-30 15:05:44 -0300

    Criado JS

---
+++ src/JavaScript/script.js
@@ -0,0 +1,20 @@
+function insert(num){
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num;
+}
+
+function clean(){
+    document.getElementById('res').innerHTML = "";
+}
+
+function back(){
+    var res = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = res.substring(0, res.length -1)
+}
+
+function calc(){
+    var res = document.getElementById('res').innerHTML;
+    if(res){
+        document.getElementById('res').innerHTML = eval(res)
+    }
+}
